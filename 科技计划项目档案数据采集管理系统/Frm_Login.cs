cb86f2b7659486aacdd291485968a7a8e747ff06
q13044256742 <1020120790@qq.com>
2017-12-01 15:45:14 +0800

    #

--- 科技计划项目档案数据采集管理系统/Frm_Login.cs
+++ 科技计划项目档案数据采集管理系统/Frm_Login.cs
@@ -11,7 +11,7 @@ namespace 科技计划项目档案数据采集管理系统
         private void btn_Login_Click(object sender, EventArgs e) {
             int i = cbo_Identity.SelectedIndex;
             i = 1;
-            //移交登记
+            ////移交登记
             if(i == 1)
             {
                 Frm_MainFrame fm = new Frm_MainFrame();
