708ed1f00dd5fb57f864b9a849105babc4371110
q13044256742 <1020120790@qq.com>
2018-01-18 17:58:29 +0800

    #1758

---
+++ 科技计划项目档案数据采集管理系统/Client.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace 科技计划项目档案数据采集管理系统
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtxt_ShowMsg = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtxt_ShowMsg
+            // 
+            this.rtxt_ShowMsg.Location = new System.Drawing.Point(64, 207);
+            this.rtxt_ShowMsg.Name = "rtxt_ShowMsg";
+            this.rtxt_ShowMsg.Size = new System.Drawing.Size(636, 186);
+            this.rtxt_ShowMsg.TabIndex = 2;
+            this.rtxt_ShowMsg.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(35, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 160);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "选择";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 405);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rtxt_ShowMsg);
+            this.Controls.Add(this.button1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtxt_ShowMsg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ 科技计划项目档案数据采集管理系统/Client.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using KyoFileEntity;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace 科技计划项目档案数据采集管理系统
+{
+    public partial class Client : Form
+    {
+        Socket _socket = null;
+        public Client()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (string item in listBox1.Items)
+            {
+                string filePath = item;
+
+                FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                byte[] _fileByte = new byte[fileStream.Length];
+                fileStream.Read(_fileByte, 0, _fileByte.Length);
+
+                FileEntity fileEntity = new FileEntity
+                {
+                    KfeId = Guid.NewGuid().ToString("N"),
+                    KfeName = Path.GetFileName(filePath),
+                    KfeFileSize = Convert.ToInt32((new FileInfo(filePath).Length / 1024)),
+                    KfeFormat = Path.GetExtension(filePath),
+                    KfeFilePath = filePath,
+                    KfeByte = _fileByte
+                };
+
+                byte[] _byte = ObjectToBytes(fileEntity);
+                _socket.Send(GetByteWithLength(_byte));
+            }
+        }
+
+        /// <summary>
+        /// 将指定字节数组长度字节数组追加到数组头部
+        /// 【固定占4个长度】
+        /// </summary>
+        /// <param name="_byte">指定的字节数组</param>
+        /// <returns>拼装后的数组</returns>
+        byte[] GetByteWithLength(byte[] _byte)
+        {
+            byte[] lengthByte = BitConverter.GetBytes(_byte.Length);
+            byte[] resultByte = new byte[_byte.Length + lengthByte.Length];
+            Buffer.BlockCopy(lengthByte, 0, resultByte, 0, lengthByte.Length);
+            Buffer.BlockCopy(_byte, 0, resultByte, lengthByte.Length, _byte.Length);
+            return resultByte;
+        }
+
+        private void ReceiveMessage(Socket socket)
+        {
+            new Thread(delegate ()
+            {
+                while (true)
+                {
+                    byte[] _byte = new byte[1024 * 1024];
+                    int length = socket.Receive(_byte);
+                    string msg = Encoding.UTF8.GetString(_byte, 0, length);
+                    ShowReceiveMsg("Server", msg);
+                }
+            }).Start();
+        }
+
+        /// <summary>
+        /// 打印接受到的信息
+        /// </summary>
+        /// <param name="talker">信息发出者</param>
+        /// <param name="msg">信息文本</param>
+        private void ShowReceiveMsg(string talker, string msg)
+        {
+            rtxt_ShowMsg.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + $"\t{talker}:\t{msg}\r\n");
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ip = "192.168.0.103";
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect(new IPEndPoint(IPAddress.Parse(ip), 8080));
+            ShowReceiveMsg("Client", "连接服务器成功");
+
+            ReceiveMessage(_socket);
+        }
+
+
+        /// <summary> 
+        /// 将一个object对象序列化，返回一个byte[]         
+        /// </summary> 
+        /// <param name="obj">能序列化的对象</param>         
+        /// <returns></returns> 
+        public static byte[] ObjectToBytes(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, obj);
+                return ms.GetBuffer();
+            }
+        }
+
+        /// <summary> 
+        /// 将一个序列化后的byte[]数组还原         
+        /// </summary>
+        /// <param name="Bytes"></param>         
+        /// <returns></returns> 
+        public static object BytesToObject(byte[] Bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(Bytes))
+            {
+                return new BinaryFormatter().Deserialize(ms);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.AddRange(fileDialog.FileNames);
+            }
+        }
+    }
+}
